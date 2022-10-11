5bdb3404b65912a27a9834253fcbed5696919243
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-10-11 15:56:25 +0100

    implement Child Domain

--- src/main/java/tn/esb/bis/childRestApi/Domains/Child.java
+++ src/main/java/tn/esb/bis/childRestApi/Domains/Child.java
@@ -1,21 +1,37 @@
 package tn.esb.bis.childRestApi.Domains;
 
-import lombok.NonNull;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Getter //génère tous les getters pour tous les attributs
+@Setter //génère tous les setters pour tous les attributs
+@NoArgsConstructor //gère un constructeur non parametré
+//@AllArgsConstructor //gnère un constructeur avec tous les attributs comme parametres
+//@EqualsAndHashCode //La comparaison entre deux objets Child se fait en comparant tous les attributs deux à deux respectivement
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)//uniquement les attributs annotés avec @EqualsAndHashCode.Include
+// seront impliqués dans la comparaison
+//@EqualsAndHashCode(exclude = "photo") //la comparaison sera effectuée via tous les attributs sauf la photo
+//@ToString //retourne une chaine contenant les valeurs de tous les attributs
+//@ToString(exclude = {"photo","birthday"}) //retourne une chaine contenant les valeurs de tous les attributs sauf photo et birthday
+@ToString(exclude = "photo") //retourne une chaine contenant les valeurs de tous les attributs sauf photo
+@RequiredArgsConstructor//genère un constructeur ayant comme paramètre uniquement les attributs annotés par @NonNull
 @Entity //La classe Child sera transformée en une table relationnelle par l'ORM
 //avec ORM : Object Relational Mapping
 public class Child {
     @Id //l'attribut "id" est la clé primaire de la table relationnelle "Child"
+   // @GeneratedValue //le champ "id" est généré automatiquemnt par le SGBD
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//les valeurs attribuées pour la clé primaire commence de 1 et sont incrémentés par 1
+    //suite à chaque ajout d'un enfant
     private Long id;
     @NonNull //ce champ est obligatoire
+    @EqualsAndHashCode.Include
     private String firstName;
     @NonNull
+    @EqualsAndHashCode.Include
+    @Column(name = "family_name",length=100)
     private String lastName;
+    @EqualsAndHashCode.Include
     private LocalDate birthDate;
     private String schoolGrade;
     @Lob
