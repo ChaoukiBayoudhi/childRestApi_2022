780cb1de31b07c2c173c241e4034ec30d57e9b49
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-10-18 13:40:10 +0100

    implement Parent Domain

--- src/main/java/tn/esb/bis/childRestApi/Domains/Parent.java
+++ src/main/java/tn/esb/bis/childRestApi/Domains/Parent.java
@@ -1,4 +1,29 @@
 package tn.esb.bis.childRestApi.Domains;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString(exclude = "photo")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Parent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @EqualsAndHashCode.Include
+    private String name;
+    @NonNull
+    @EqualsAndHashCode.Include
+    private String familyName;
+    @NonNull
+    @EqualsAndHashCode.Include
+    private LocalDate birthDate;
+    @Lob
+    private  byte[] photo;
 }
